3e128857078b036beac82b6d231edd01ab60880c
gortiz <ggortiz09@gmail.com>
2021-05-10 23:57:51 -0300

    fix main security

--- src/main/java/com/gortiz/Springjwt/MainSecurity.java
+++ src/main/java/com/gortiz/Springjwt/MainSecurity.java
@@ -2,12 +2,16 @@ package com.gortiz.Springjwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,6 +19,9 @@ import com.gortiz.Springjwt.jwt.JwtEntryPoint;
 import com.gortiz.Springjwt.jwt.JwtTokenFilter;
 import com.gortiz.Springjwt.service.UserDetailsServiceImpl;
 
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MainSecurity  extends WebSecurityConfigurerAdapter{
 
 	@Autowired
