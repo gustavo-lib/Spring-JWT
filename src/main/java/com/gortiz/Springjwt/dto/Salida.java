eaab641c90d3653c1d7c0a49fd4b2128ce440dfa
gortiz <ggortiz09@gmail.com>
2021-05-10 23:02:51 -0300

    class salida mensaje

---
+++ src/main/java/com/gortiz/Springjwt/dto/Salida.java
@@ -0,0 +1,17 @@
+package com.gortiz.Springjwt.dto;
+
+public class Salida {private String mensaje;
+
+public Salida(String mensaje) {
+    this.mensaje = mensaje;
+}
+
+public String getSalida() {
+    return mensaje;
+}
+
+public void setSalida(String mensaje) {
+    this.mensaje = mensaje;
+}
+
+}
